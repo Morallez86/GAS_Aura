d93db3e79547db554b4ae48a31b5e490fec2ae2c
Morallez86 <jjoaommorais@hotmail.com>
2025-06-06 15:09:07 +0100

    Player controller. Highlight enemies. Player state

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,23 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	
+	GetMesh()->SetRenderCustomDepth(true); // Enable custom depth rendering for highlighting
+    GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED); // Set a custom stencil value for the mesh to be highlighted
+    Weapon->SetRenderCustomDepth(true); // Enable custom depth rendering for the weapon
+    Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED); // Set a custom stencil value for the weapon to be highlighted
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	
+	GetMesh()->SetRenderCustomDepth(false); // Disable custom depth rendering to remove highlighting
+    Weapon->SetRenderCustomDepth(false); // Disable custom depth rendering for the weapon
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -10,6 +10,7 @@
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class IEnemyInterface;
 /**
  * 
  */
@@ -20,6 +21,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
@@ -35,7 +37,15 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	void CursorTrace();
+
+	TScriptInterface<IEnemyInterface> LastActor;
+	TScriptInterface<IEnemyInterface> ThisActor;
+
 };
+
+
 
 
 
